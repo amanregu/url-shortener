0a51eaa23fad58e8f83794d67d18bac97e705337
Aman Regu <amanregu@gmail.com>
2020-05-16 23:56:18 +0530

    Add URL model

---
+++ app/models/url.rb
@@ -0,0 +1,12 @@
+class Url < ApplicationRecord
+  validates :original, presence: true, format: { with: URI::regexp(%w(http https)),
+             message: "Enter a valid URL." }, uniqueness: true
+  validates :slug, presence: true, uniqueness: true, length: { is: 8 }
+
+  def generate_slug
+    loop do
+      self.slug = SecureRandom.alphanumeric(8)
+      break unless Url.exists?(slug: slug)
+    end
+  end
+end
