3f6ef779ecd51d3f7a0e3cb8e840b609f0ca00ef
Aman Regu <amanregu@gmail.com>
2020-05-17 06:27:41 +0530

    Add url controller

---
+++ app/controllers/urls_controller.rb
@@ -0,0 +1,37 @@
+class UrlsController < ApplicationController
+skip_before_action :verify_authenticity_token
+
+  def encode
+    @url = Url.find_by(url_params)
+
+    if @url 
+      render status: :ok, json: { slug: @url.slug }
+    else
+      @url = Url.new(url_params)
+      @url.generate_slug
+
+      if @url.save
+        render status: 	:created, json: { slug: @url.slug }
+      else
+        render status: :unprocessable_entity, json: { errors: @url.errors.full_messages }
+      end
+
+    end
+  end
+
+  def decode
+    @url = Url.find_by(url_params)
+
+    if @url
+      render status: :ok, json: { original: @url.original }
+    else
+      render status: :not_found
+    end
+  end
+
+
+  private
+    def url_params
+      params.require(:url).permit(:original, :slug)
+    end
+end
