8d6928a5b2146b3fd23d20c8e4b765f7beaa8f08
Aman Regu <amanregu@gmail.com>
2020-05-19 17:31:29 +0530

    Add list component

--- app/javascript/components/urls/List.js
+++ app/javascript/components/urls/List.js
@@ -1,13 +1,30 @@
 import React from "react"
 import PropTypes from "prop-types"
-class List extends React.Component {
-  render () {
-    return (
-      <React.Fragment>
-        <h1>Hello from React</h1>
-      </React.Fragment>
-    );
-  }
+
+const List = ({ urls }) => {
+  return (
+    <>
+      <h1>URL List</h1>
+      <div className="table-container" >
+        <table>
+          {urls.map((url, index) => {
+            return (
+              <>
+              <tr>
+                <td style={ {display: "block"}} key={index} >
+                    {url.original}
+                  </td>
+                  <td>
+                    {url.slug}
+                  </td>
+              </tr>
+              </>
+            )
+          })}
+        </table>
+      </div>
+    </>
+  )
 }
 
 export default List
