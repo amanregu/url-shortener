c939e456b912b9bd2bdb27a7e77c339fcdba814a
Aman Regu <amanregu@gmail.com>
2020-05-31 17:25:20 +0530

    Add urls association to category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_23_111914) do
+ActiveRecord::Schema.define(version: 2020_05_31_115021) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -24,8 +24,11 @@ ActiveRecord::Schema.define(version: 2020_05_23_111914) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "is_pinned", default: false, null: false
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_urls_on_category_id"
     t.index ["original"], name: "index_urls_on_original", unique: true
     t.index ["slug"], name: "index_urls_on_slug", unique: true
   end
 
+  add_foreign_key "urls", "categories"
 end
