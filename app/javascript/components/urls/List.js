562a1b5881bd5099ba4175cda3d1b181386543a3
Aman Regu <amanregu@gmail.com>
2020-05-31 21:31:13 +0530

    Add functionality to assign category to urls

--- app/javascript/components/urls/List.js
+++ app/javascript/components/urls/List.js
@@ -3,6 +3,9 @@ import Pin from "./Pin"
 
 const List = (props) => {
   const [urls, setUrls] = useState(props.urls)
+  const [categories] = useState(props.categories)
+  console.log(urls)
+  console.log(categories)
 
   const handleClick = (slug, is_pinned) => {
     fetch(`/urls/${slug}`, {
@@ -19,15 +22,35 @@ const List = (props) => {
       })
     }).then(res => res.json()).then(res => setUrls(res.urls))
   }
+
+  const updateCategory = (e, slug) => {
+    fetch(`/urls/${slug}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRF-TOKEN': document.querySelector('[name="csrf-token"]').content
+      },
+      body: JSON.stringify({
+        "url": {
+          category_id: e.target.value
+        }
+      })
+    }).then(res => res.json()).then(res => setUrls(res.urls))
+  }
+
   return (
     <>
-      <h1 className="text-center" >List Of Shortened URLs</h1>
       <div className="container" >
+        <div className="d-flex flex-row justify-content-between" >
+          <h1 className="text-center" >List Of Shortened URLs</h1>
+          <a>Manage Categories</a>
+        </div>
         <table className="table table-bordered">
           <thead className="thead-dark" >
             <tr>
               <th scope="col">#</th>
               <th scope="col">Original URL</th>
+              <th scope="col">Category</th>
               <th scope="col">Short URL</th>
             </tr>
           </thead>
@@ -37,11 +60,29 @@ const List = (props) => {
               <>
                 <tbody>
                   <tr>
-                    <td style={{ cursor: 'pointer' }}  onClick={() => handleClick(url.slug, url.is_pinned)}>
+                    <td style={{ cursor: 'pointer' }} onClick={() => handleClick(url.slug, url.is_pinned)}>
                       <Pin isPinned={url.is_pinned} />
                     </td>
                     <td className="col">
                       <a href={url.original} target="_blank" >{url.original}</a>
+                    </td>
+                    <td className="col">
+                      <div>
+                        <select onChange={(e) => updateCategory(e, url.slug)} value={categories ? url.category_id : "default"}>
+                          <option value="default">
+                            Select
+                            </option>
+                          {categories.map((category) => {
+                            return (
+                              <>
+                                <option value={category.id}>
+                                  {category.title}
+                                </option>
+                              </>
+                            )
+                          })}
+                        </select>
+                      </div>
                     </td>
                     <td className="col">
                       <a href={shorted_url} target="_blank" >{shorted_url}</a>
