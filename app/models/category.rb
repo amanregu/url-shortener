540b7e435dad912e04f116c89d97c62488048ce0
Aman Regu <amanregu@gmail.com>
2020-06-01 12:27:37 +0530

    Update validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
   has_many :urls, dependent: :nullify
-  validates :title, presence: true, uniqueness: true
+  validates :title, presence: true, uniqueness: { case_sensitive: false }
 end
