b6ba737969cd75ae44803982e5c45cbac1f0e9c3
Aman Regu <amanregu@gmail.com>
2020-06-02 20:51:00 +0530

    generate clicks controller

---
+++ app/controllers/api/v1/clicks_controller.rb
@@ -0,0 +1,4 @@
+class Api::V1::ClicksController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'clicks/index'
   namespace :api do
     namespace :v1 do
       resources :categories, only: [:index, :create, :update, :destroy]
       resources :urls, only: [:index, :create, :show, :update], param: :slug
+      resources :clicks, only: :index
     end
   end
   root "api/v1/urls#index"
