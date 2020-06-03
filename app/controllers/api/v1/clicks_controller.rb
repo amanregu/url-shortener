00a476112c85ec58dad6ed8d647d24989c465119
Aman Regu <amanregu@gmail.com>
2020-06-04 03:04:22 +0530

    Add routes to track clicks

--- app/controllers/api/v1/clicks_controller.rb
+++ app/controllers/api/v1/clicks_controller.rb
@@ -1,4 +1,30 @@
 class Api::V1::ClicksController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  before_action :fetch_clicks, only: [:create, :index]
+
   def index
+    clicks_with_date = fetch_clicks.group_by { |click| click.created_at.strftime("%B %Y") }
+    clicks_with_url_id = fetch_clicks.group_by { |click| click.url_id }
+    render status: :ok, json: { clicks_with_date: clicks_with_date, clicks_with_url_id: clicks_with_url_id }
+  end
+
+  def create
+    @click = Click.new(url_id: click_params[:url_id])
+
+    if @click.save
+      render status: :ok, json: { click: @clicks }
+    else
+      render status: :unprocessable_entity, json: { errors: @url.errors.full_messages }
+    end
+  end
+
+  private
+
+  def click_params
+    params.require(:click).permit(:url_id)
+  end
+
+  def fetch_clicks
+    @clicks = Click.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'clicks/index'
   namespace :api do
     namespace :v1 do
       resources :categories, only: [:index, :create, :update, :destroy]
       resources :urls, only: [:index, :create, :show, :update], param: :slug
-      resources :clicks, only: :index
+      resources :clicks, only: [:index, :create]
     end
   end
   root "api/v1/urls#index"
